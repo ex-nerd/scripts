92b6fe796e2af56ff1ce88b5ee6bf986f49cfdf8
Chris Petersen <geek+github@ex-nerd.com>
2011-09-28 23:59:21 -0700

    Lots of tweaks/cleanup

--- fixm4a.py
+++ fixm4a.py
@@ -39,46 +39,145 @@ tags = {
 def strip_accents(s):
    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
+def fix_name(s):
+    new = strip_accents(s)
+    new = re.sub('\(.+\)', '', new)
+    new = re.sub('\[.+\]', '', new)
+    new = new.replace(u'\u2019', "'") # smart apostrophe
+    new = new.replace(u'\xb4', "'")   # some form of tilde
+    new = new.replace(u'\xa1', '')    # upside down !
+    new = new.replace(u'\xe6', 'ae')
+    new = new.replace(u'\xdf', 'ss')  # eszet
+    new = new.replace(u'\xa1', '')    # upside down !
+    new = new.replace(u'\xbf', '')    # upside down ?
+    new = new.replace(' MI ', ' Mi ') # typo
+    new = new.replace('"', '')
+    new = re.sub('\s*&\s*', ' and ', new)
+    new = re.sub(r'[\\/:*?<>|]+', '-', new) # characters unfriendly to win/mac
+    new = re.sub(r' -(?! )', ' ', new)
+    new = re.sub(r'\s+', ' ', new)
+    new = re.sub(r'\W+;', ';', new)
+    new = re.sub(r'^\W+|\W+$', '', new)
+    new = re.sub(r'\W+(\.\w+)$', r'\1', new)
+    new = new.replace(' #!@-', ' Shit') # for one specific song
+    new = re.sub(r'^(\d+)\. ', r'\1 ', new)
+    return new
+
+def load_mp4(dir, file, _disk = None):
+    m = MP4(os.path.join(dir, file))
+    changed = False
+    # Don't want this taking up space
+    if m[tags['encodedby']] != ['']:
+        m[tags['encodedby']] = ['']
+        changed = True
+    # Adjust num tracks
+    try:
+        (track, numtracks) = m['trkn'][0]
+    except:
+        track = numtracks = 0
+    if numtracks == 0:
+        numtracks = len(music)
+        m['trkn'] = [(track,numtracks)]
+        changed = True
+    # Try to detect disk numbers
+    try:
+        (disk, numdisks) = m['disk'][0]
+    except:
+        disk = numdisks = 0
+    if _disk and disk != _disk:
+        print "Disknum mismatch on {0}".format(file)
+        sys.exit(1)
+    # Clean up the title
+    #print m[tags['title']][0]
+    title = m[tags['title']][0].strip()
+    title = title.replace(',,,', ';')  # catch some oddities left over from fixing nero's tag issues
+    title = re.sub(r'\s+', ' ', title)
+    title = title.replace(' MI ', ' Mi ') # typo
+    for term in ('the', 'by', 'of', 'a', 'an', 'and', 'but', 'or', 'for', 'nor', 'to', 'at', 'de', 'la', 'los', 'del', 'von', 'in', 'des', 'dem', 'der', 'di', 'with'):
+        title = re.sub(
+            r' {0} '.format(term),
+            r' {0} '.format(term),
+            title, flags=re.IGNORECASE
+            )
+        title = re.sub(
+            r'([^a-z,]) {0} '.format(term),
+            r'\1 {0} '.format(term.title()),
+            title, flags=re.IGNORECASE
+            )
+        title = title.replace(' a Minor', ' A Minor')
+    title = re.sub(r'(?:#|No\.?)(\d)', r'No. \1', title)
+    title = title.replace(u'\u2019', "'") # smart apostrophe
+    title = title.replace(u'\xb4', "'")   # some form of tilde meant to be an apostrophe
+    if title != m[tags['title']][0]:
+        m[tags['title']] = [title]
+        changed = True
+    # Generate a new name
+    new = fix_name(u"{0} {1}.m4a".format(
+        str(track).zfill(max(2,len(str(numtracks)))),
+        title
+        ))
+    if disk > 0 and numdisks > 1:
+        new = "{0}-{1}".format(
+            str(disk).zfill(len(str(numdisks))),
+            new)
+    try:
+        if file.decode("utf-8") == new.decode("utf-8"):
+            new = None
+    except:
+        print "FILE:  "+os.path.join(dirname, file)
+        raise
+    # Return
+    return (m, new, changed)
+
 def visit(arg, dirname, names):
-    #print dirname
+    print dirname
     # Do some math/checking for disk number info
-    parent = os.path.dirname(dirname)
+    #parent = os.path.dirname(dirname)
     thisdir = os.path.basename(dirname)
-    if 'Disk ' in thisdir:
-        # Try to find other matching disk directories so we can determine a max number
-        pass
+    m = re.match(r'Disk ([ABC]|\d+)', thisdir)
+    if m:
+        disknum = m.group(1)
+        if disknum == 'A':
+            disknum = 1
+        elif disknum == 'B':
+            disknum = 2
+        elif disknum == 'C':
+            disknum = 3
     # Parse the files
-    music = []
+    music   = []
+    artists = {}
+    is_comp = False
+    artist  = None
     for file in names:
         if file.startswith('.'):
             continue
         if file.endswith('.m4a'):
+            # Load some info about this mp4 for later comparison
+            (mp4, name, changed) = load_mp4(dirname, file)
+            # Setup for compilation detection
+            if not is_comp:
+                if artist:
+                    if artist != mp4[tags['artist']][0]:
+                        is_comp = True
+                else:
+                    artist = mp4[tags['artist']][0]
+                    if artist.lower() == 'various':
+                        is_comp = True
+                    else:
+                        a_artist = mp4.get(tags['albumartist'], [None])[0]
+                        if a_artist and a_artist != artist:
+                            is_comp = True
             # Save music files for later
-            music.append(file)
+            music.append({
+                'file':    file,
+                'name':    name,
+                'mp4':     mp4,
+                'changed': changed,
+                })
         elif not os.path.isdir(os.path.join(dirname, file)):
             try:
-                # Rename to remove UTF-8 characters so OSX can see them over NFS
-                new = strip_accents(file.decode("utf-8"))
-                new = new.replace(u'\u2019', "'") # smart apostrophe
-                new = new.replace(u'\xb4', "'")   # some form of tilde
-                new = new.replace(u'\xa1', '')    # upside down !
-                new = new.replace(u'\xe6', 'ae')
-                new = new.replace(u'\xdf', 'ss')  # eszet
-                new = new.replace(u'\xa1', '')    # upside down !
-                new = new.replace(u'\xbf', '')    # upside down ?
-                new = new.replace(' MI ', ' Mi ') # typo
-                new = re.sub('\(.+\)', '', new)
-                new = re.sub('\[.+\]', '', new)
-                new = new.replace('"', '')
-                new = re.sub('\s*&\s*', ' and ', new)
-                new = re.sub(r'[\\/:*?<>|]+', '-', new)
-                new = re.sub(r' -(?! )', ' ', new)
-                new = re.sub(r'\s+', ' ', new)
-                new = re.sub(r'\W+;', ';', new)
-                new = re.sub(r'^\W+|\W+$', '', new)
-                new = re.sub(r'\W+(\.\w+)$', r'\1', new)
-                new = new.replace(' #!@-', ' Shit')
-                new = re.sub(r'^(\d+)\. ', r'\1 ', new)
+                # Clean up the name
+                new = fix_name(file.decode("utf-8"))
                 #print os.path.join(dirname, file)
                 if file.decode("utf-8") != new.decode("utf-8"):
                     print '{0}\n  {1}'.format(file, new)
@@ -86,85 +185,32 @@ def visit(arg, dirname, names):
             except:
                 print "FILE:  "+os.path.join(dirname, file)
                 raise
+    if len(artists) > 1:
+        is_comp = True
     # Now parse the music files that we found
-    for file in music:
-        changed = False
-        # Parse the m4a tag
-        m = MP4(os.path.join(dirname, file))
-        m[tags['encodedby']] = ['']
-        try:
-            (disk, numdisks) = m['disk'][0]
-        except:
-            disk = numdisks = 0
-        try:
-            (track, numtracks) = m['trkn'][0]
-        except:
-            track = numtracks = 0
-        if numtracks == 0:
-            numtracks = len(music)
-            m['trkn'] = [(track,numtracks)]
+    for group in music:
+        # Load the data from the group, for easier manipulation
+        file    = group['file']
+        name    = group['name']
+        mp4     = group['mp4']
+        changed = group['changed']
+        #print file
+        # Compilation?
+        compilation = mp4.get(tags['partofcompilation'])
+        if not compilation and is_comp:
+            mp4[tags['partofcompilation']] = True
             changed = True
-        # Clean up the title
-        #print m[tags['title']][0]
-        title = m[tags['title']][0].strip()
-        title = title.replace(',,,', ';')  # catch some oddities left over from fixing nero's tag issues
-        title = re.sub(r'\s+', ' ', title)
-        title = title.replace(' MI ', ' Mi ') # typo
-        for term in ('the', 'by', 'of', 'a', 'an', 'and', 'but', 'or', 'for', 'nor', 'to', 'at', 'de', 'la', 'los', 'del', 'von', 'in', 'des', 'dem', 'der', 'di', 'with'):
-            title = re.sub(
-                r' {0} '.format(term),
-                r' {0} '.format(term),
-                title, flags=re.IGNORECASE
-                )
-            title = re.sub(
-                r'([^a-z,]) {0} '.format(term),
-                r'\1 {0} '.format(term.title()),
-                title, flags=re.IGNORECASE
-                )
-            title = title.replace(' a Minor', ' A Minor')
-        title = re.sub(r'(?:#|No\.?)(\d)', r'No. \1', title)
-        title = title.replace(u'\u2019', "'") # smart apostrophe
-        title = title.replace(u'\xb4', "'")   # some form of tilde
-        title = title.replace(u'\xe6', 'ae')
-        if title != m[tags['title']][0]:
-            m[tags['title']] = [title]
+        a_artist = mp4.get(tags['albumartist'], [None])[0]
+        if compilation and not a_artist:
+            mp4[tags['albumartist']] = ['Various']
             changed = True
-        #print title
-        # Other tags we may someday want to clean up
-        #m[tags['artist']]
-        #m[tags['albumartist']]
-        #m[tags['partofcompilation']]
         # Save
         if changed:
-            m.save()
-        # Generate a new name
-        new = u"{0} {1}".format(
-            str(track).zfill(max(2,len(str(numtracks)))),
-            title
-            )
-        new = new.replace(u'\xe6', 'ae')
-        new = new.replace(u'\xdf', 'ss')  # eszet
-        new = new.replace(u'\xa1', '')    # upside down !
-        new = new.replace(u'\xbf', '')    # upside down ?
-        new = strip_accents(new)
-        new = re.sub('\(.+\)', '', new)
-        new = re.sub('\[.+\]', '', new)
-        new = new.replace('"', '')
-        new = re.sub('\s*&\s*', ' and ', new)
-        new = re.sub(r'[\\/:*?<>|]+', '-', new)
-        new = re.sub(r' -(?! )', ' ', new)
-        new = re.sub(r'\s+', ' ', new)
-        new = re.sub(r'\W+;', ';', new)
-        new = re.sub(r'^\W+|\W+$', '', new)
-        new = new.replace(' #!@-', ' Shit')
-        new = new.strip()+'.m4a'
-        try:
-            if file.decode("utf-8") != new.decode("utf-8"):
-                print '{0}\n  {1}'.format(file, new)
-                os.rename(os.path.join(dirname, file), os.path.join(dirname, new))
-        except:
-            print "FILE:  "+os.path.join(dirname, file)
-            raise
+            print "Save {0}".format(file)
+            mp4.save()
+        if name:
+            print '{0}\n  {1}'.format(file, name)
+            os.rename(os.path.join(dirname, file), os.path.join(dirname, name))
 
 if 1 in sys.argv:
     path = os.path.abspath(sys.argv[1])
