4e317eb2620a58b4738ff19c725166622b4fd101
Chris Petersen <geek+github@ex-nerd.com>
2011-10-01 02:25:32 -0700

    mp3 support, lots more cleanup and goodness

--- fixm4a.py
+++ fixm4a.py
@@ -1,41 +1,95 @@
 #!/usr/bin/python
+#
+# Script used to clean up my music collection.  Most of the understanding I
+# gained about mutagen comes thanks to puddletag.  I borrowed some of the
+# tag name->field mappings, so this script might as well be licensed under
+# the GPLv3, just like puddletag.
+#
 
-import os, sys, re
-from mutagen.mp4 import MP4
-import unicodedata
-
+# Set to true for test mode
 dryrun = False
 
-tags = {
-    'album': '\xa9alb',
-    'albumartist': 'aART',
+import os, sys, re
+import unicodedata
+from mutagen.mp4 import MP4
+import mutagen.id3 as id3
+
+t4 = {
+    'album':                '\xa9alb',
+    'albumartist':          'aART',
     'albumartistsortorder': 'soaa',
-    'albumsortorder': 'soal',
-    'artist': '\xa9ART',
-    'artistsortorder': 'soar',
-    'bpm': 'tmpo',
-    'comment': '\xa9cmt',
-    'composer': '\xa9wrt',
-    'composersortorder': 'soco',
-    'copyright': 'cprt',
-    'description': 'desc',
-    'encodedby': '\xa9too',
-    'genre': '\xa9gen',
-    'grouping': '\xa9grp',
-    'lyrics': '\xa9lyr',
-    'partofcompilation': 'cpil',
-    'partofgaplessalbum': 'pgap',
-    'podcast': 'pcst',
-    'podcastcategory': 'catg',
-    'podcastepisodeguid': 'egid',
-    'podcastkeywords': 'keyw',
-    'podcasturl': 'purl',
-    'purchasedate': 'purd',
-    'showname': 'tvsh',
-    'showsortorder': 'sosn',
-    'title': '\xa9nam',
-    'titlesortorder': 'sonm',
-    'year': '\xa9day',
+    'albumsortorder':       'soal',
+    'artist':               '\xa9ART',
+    'artistsortorder':      'soar',
+    'bpm':                  'tmpo',
+    'comment':              '\xa9cmt',
+    'composer':             '\xa9wrt',
+    'composersortorder':    'soco',
+    'copyright':            'cprt',
+    'description':          'desc',
+    'encodedby':            '\xa9too',
+    'genre':                '\xa9gen',
+    'grouping':             '\xa9grp',
+    'lyrics':               '\xa9lyr',
+    'partofcompilation':    'cpil',
+    'partofgaplessalbum':   'pgap',
+    'podcast':              'pcst',
+    'podcastcategory':      'catg',
+    'podcastepisodeguid':   'egid',
+    'podcastkeywords':      'keyw',
+    'podcasturl':           'purl',
+    'purchasedate':         'purd',
+    'showname':             'tvsh',
+    'showsortorder':        'sosn',
+    'title':                '\xa9nam',
+    'titlesortorder':       'sonm',
+    'year':                 '\xa9day',
+    }
+
+t3 = {
+    'album':              'TALB',
+    'bpm':                'TBPM',
+    'composer':           'TCOM',
+    'copyright':          'TCOP',
+    'date':               'TDAT',
+    'audiodelay':         'TDLY',
+    'encodedby':          'TENC',
+    'lyricist':           'TEXT',
+    'filetype':           'TFLT',
+    'time':               'TIME',
+    'grouping':           'TIT1',
+    'title':              'TIT2',
+    'version':            'TIT3',
+    'initialkey':         'TKEY',
+    'language':           'TLAN',
+    'audiolength':        'TLEN',
+    'mediatype':          'TMED',
+    'mood':               'TMOO',
+    'originalalbum':      'TOAL',
+    'filename':           'TOFN',
+    'author':             'TOLY',
+    'originalartist':     'TOPE',
+    'originalyear':       'TORY',
+    'fileowner':          'TOWN',
+    'artist':             'TPE1',
+    'albumartist':        'TPE2',
+    'conductor':          'TPE3',
+    'arranger':           'TPE4',
+    'disk':               'TPOS',
+    'producednotice':     'TPRO',
+    'organization':       'TPUB',
+    'track':              'TRCK',
+    'recordingdates':     'TRDA',
+    'radiostationname':   'TRSN',
+    'radioowner':         'TRSO',
+    'audiosize':          'TSIZ',
+    'albumsortorder':     'TSOA',
+    'performersortorder': 'TSOP',
+    'titlesortorder':     'TSOT',
+    'isrc':               'TSRC',
+    'encodingsettings':   'TSSE',
+    'setsubtitle':        'TSST',
+    'year':               'TYER',
     }
 
 def strip_accents(s):
@@ -52,6 +106,13 @@ def _uppercase(m):
 
 def _musiccase(m):
     return m.group(1).title() + m.group(2).lower()
+
+def fix_album(s):
+    s = re.sub(r'\W+Dis[kc]\s+([AB]|\d+)\W*$', '', s)
+    s = re.sub(r'\W+cd\s*\d+\W*$', '', s, flags=re.IGNORECASE)
+    #if re.match(r'Dis[kc]', s) or re.match(r'cd\s*\d', s, flags=re.IGNORECASE):
+    #    print "  WARNING:  {0}".format(s)
+    return s
 
 def fix_title(s):
     new = s.strip()
@@ -111,12 +172,12 @@ def fix_name_full(new):
     new = fix_name(new)
     return new
 
-def load_mp4(dir, file, _disk = None):
+def load_mp4(dir, file, _numtracks = None, _disk = None, _numdisks = None):
     m = MP4(os.path.join(dir, file))
     changed = False
     # Don't want this taking up space
-    if m.get(tags['encodedby'], []) != ['']:
-        m[tags['encodedby']] = ['']
+    if m.get(t4['encodedby'], []) != ['']:
+        m[t4['encodedby']] = ['']
         changed = True
     # Adjust num tracks
     try:
@@ -124,24 +185,41 @@ def load_mp4(dir, file, _disk = None):
     except:
         track = numtracks = 0
     if numtracks == 0:
-        numtracks = len(music)
-        m['trkn'] = [(track,numtracks)]
-        changed = True
+        if _numtracks:
+            numtracks = _numtracks
+            m['trkn'] = [(track,numtracks)]
+            changed = True
+        else:
+            print "  no numtracks for {0}".format(file)
+            sys.exit(1)
     # Try to detect disk numbers
     try:
         (disk, numdisks) = m['disk'][0]
     except:
         disk = numdisks = 0
-    if _disk and disk != _disk:
-        print "  Disknum mismatch on {0}".format(file)
+    if _disk and _numdisks and not disk and not numdisks:
+        disk     = _disk
+        numdisks = _numdisks
+        m['disk'] = [(disk,numdisks)]
+        changed = True
+    elif _disk and disk != _disk:
+        print "  Disknum mismatch {0} != {1} on {2}".format(disk, _disk, file)
         sys.exit(1)
+    elif disk and not numdisks:
+        print "  disknum but no numdisks on {0}".format(file)
+        sys.exit(1)
+    # Clean up the album name
+    album = fix_album(m[t4['album']][0].strip())
+    if album != m[t4['album']][0]:
+        m[t3['album']] = [album]
+        changed = True
     # Clean up the title
-    #print m[tags['title']][0]
-    title = m[tags['title']][0].strip()
+    #print m[t4['title']][0]
+    title = m[t4['title']][0].strip()
     title = title.replace(',,,', ';')  # catch some oddities left over from fixing nero's tag issues
     title = fix_title(title)
-    if title != m[tags['title']][0]:
-        m[tags['title']] = [title]
+    if title != m[t4['title']][0]:
+        m[t4['title']] = [title]
         changed = True
     # Generate a new name
     new = fix_name_full(u"{0} {1}.m4a".format(
@@ -161,51 +239,120 @@ def load_mp4(dir, file, _disk = None):
     # Return
     return (m, new, changed)
 
+def load_mp3(dir, file, _numtracks = None, _disk = None, _numdisks = None):
+    m = id3.ID3(os.path.join(dir, file))
+    changed = False
+    # Don't want this taking up space
+    if t3['encodedby'] in m:
+        del(m[t3['encodedby']])
+        changed = True
+    # Adjust num tracks
+    try:
+        numtracks = 0
+        track = m[t3['track']].text[0]
+        if '/' in track:
+            (track, numtracks) = track.split('/')
+    except:
+        track = numtracks = 0
+    if numtracks == 0:
+        if _numtracks:
+            numtracks = _numtracks
+            m[t3['track']] = id3.TRCK(0, '{0}/{1}'.format(track,numtracks))
+            changed = True
+        else:
+            print "  no numtracks for {0}".format(file)
+            sys.exit(1)
+    # Try to detect disk numbers
+    try:
+        numdisks = 0
+        disk = m[t3['disk']].text[0]
+        if '/' in disk:
+            (disk, numdisks) = disk.split('/')
+    except:
+        disk = numdisks = 0
+    if _disk and _numdisks and not int(disk) and not int(numdisks):
+        disk     = str(_disk).zfill(len(str(_numdisks)))
+        numdisks = _numdisks
+        m[t3['disk']] = id3.TPOS(0, '{0}/{1}'.format(disk,numdisks))
+        changed = True
+    elif _disk and disk != _disk:
+        print "  Disknum mismatch {0} != {1} on {2}".format(disk, _disk, file)
+        sys.exit(1)
+    elif int(disk) and not int(numdisks):
+        if _numdisks:
+            disk     = str(disk).zfill(len(str(_numdisks)))
+            numdisks = _numdisks
+            m[t3['disk']] = id3.TPOS(0, '{0}/{1}'.format(disk,numdisks))
+            changed = True
+        else:
+            print "  disknum but no numdisks on {0}".format(file)
+            sys.exit(1)
+    # Clean up the album name
+    if t3['album'] in m:
+        album = fix_album(m[t3['album']].text[0].strip())
+        if album != m[t3['album']].text[0]:
+            m[t3['album']].text[0] = album
+            changed = True
+    # Clean up the title
+    #print m[t3['title']].text[0]
+    title = m[t3['title']].text[0].strip()
+    title = fix_title(title)
+    if title != m[t3['title']].text[0]:
+        m[t3['title']].text = [title]
+        changed = True
+    # Generate a new name
+    new = fix_name_full(u"{0} {1}.mp3".format(
+        str(track).zfill(max(2,len(str(numtracks)))),
+        title
+        ))
+    if int(disk) > 0 and int(numdisks) > 1:
+        new = "{0}-{1}".format(
+            str(disk).zfill(len(str(numdisks))),
+            new)
+    try:
+        if file.decode("utf-8") == new.decode("utf-8"):
+            new = None
+    except:
+        print "  FILE:  "+os.path.join(dirname, file)
+        raise
+    # Return
+    return (m, new, changed)
+
 def visit(arg, dirname, names):
     print dirname
     # Do some math/checking for disk number info
-    #parent = os.path.dirname(dirname)
+    parent = os.path.dirname(dirname)
     thisdir = os.path.basename(dirname)
-    m = re.match(r'Disk ([ABC]|\d+)', thisdir)
+    m = re.match(r'Disk ([AB]|\d+)\b', thisdir)
+    disknum  = 0
+    numdisks = 0
     if m:
         disknum = m.group(1)
         if disknum == 'A':
             disknum = 1
         elif disknum == 'B':
             disknum = 2
-        elif disknum == 'C':
-            disknum = 3
+        for dir in os.listdir(parent):
+            if not os.path.isdir(os.path.join(parent, dir)):
+                continue
+            m = re.match(r'Dis[ck]\s+([A-D]|\d+)\b', dir)
+            if m:
+                d = m.group(1)
+                if d == 'A':
+                    d = 1
+                elif d == 'B':
+                    d = 2
+                numdisks = max(numdisks, d)
     # Parse the files
-    music   = []
-    artists = {}
-    is_comp = False
-    artist  = None
+    mp3 = []
+    m4a = []
     for file in names:
         if file.startswith('.'):
             continue
-        if file.endswith('.m4a'):
-            # Load some info about this mp4 for later comparison
-            (mp4, name, changed) = load_mp4(dirname, file)
-            # Setup for compilation detection
-            if not is_comp:
-                if artist:
-                    if artist != mp4[tags['artist']][0]:
-                        is_comp = True
-                else:
-                    artist = mp4[tags['artist']][0]
-                    if artist.lower() == 'various':
-                        is_comp = True
-                    else:
-                        a_artist = mp4.get(tags['albumartist'], [None])[0]
-                        if a_artist and a_artist != artist:
-                            is_comp = True
-            # Save music files for later
-            music.append({
-                'file':    file,
-                'name':    name,
-                'mp4':     mp4,
-                'changed': changed,
-                })
+        elif file.endswith('.m4a'):
+            m4a.append(file)
+        elif file.endswith('.mp3'):
+            mp3.append(file)
         else:
             try:
                 # Clean up the name
@@ -221,6 +368,56 @@ def visit(arg, dirname, names):
             except:
                 print "  FILE:  "+os.path.join(dirname, file)
                 raise
+    # Now, parse the music files
+    music   = []
+    artists = {}
+    is_comp = False
+    artist  = None
+    for file in m4a:
+        # Load some info about this mp4 for later comparison
+        (mp4, name, changed) = load_mp4(dirname, file, len(m4a), int(disknum), int(numdisks))
+        # Setup for compilation detection
+        if not is_comp:
+            if artist:
+                if artist != mp4[t4['artist']][0]:
+                    is_comp = True
+            else:
+                artist = mp4[t4['artist']][0]
+                if artist.lower() == 'various':
+                    is_comp = True
+            a_artist = mp4.get(t4['albumartist'], [None])[0]
+            if a_artist and artist and a_artist != artist:
+                is_comp = True
+        # Save music files for later
+        music.append({
+            'file':    file,
+            'name':    name,
+            'mp4':     mp4,
+            'changed': changed,
+            })
+    for file in mp3:
+        # Load some info about this mp4 for later comparison
+        (mp3, name, changed) = load_mp3(dirname, file, len(mp3), disknum, numdisks)
+        # Setup for compilation detection
+        if not is_comp:
+            if artist:
+                if t3['artist'] in mp3 and artist != mp3[t3['artist']].text[0]:
+                    is_comp = True
+            else:
+                artist = mp3[t3['artist']].text[0]
+                if artist.lower() == 'various':
+                    is_comp = True
+            if t3['albumartist'] in mp3:
+                a_artist = mp3[t3['albumartist']].text[0]
+                if a_artist and artist and a_artist != artist:
+                    is_comp = True
+        # Save music files for later
+        music.append({
+            'file':    file,
+            'name':    name,
+            'mp3':     mp3,
+            'changed': changed,
+            })
     if len(artists) > 1:
         is_comp = True
     # Now parse the music files that we found
@@ -228,23 +425,31 @@ def visit(arg, dirname, names):
         # Load the data from the group, for easier manipulation
         file    = group['file']
         name    = group['name']
-        mp4     = group['mp4']
         changed = group['changed']
         #print file
-        # Compilation?
-        compilation = mp4.get(tags['partofcompilation'])
-        if not compilation and is_comp:
-            mp4[tags['partofcompilation']] = True
-            changed = True
-        a_artist = mp4.get(tags['albumartist'], [None])[0]
-        if compilation and not a_artist:
-            mp4[tags['albumartist']] = ['Various']
-            changed = True
+        if 'mp4' in group:
+            m = group['mp4']
+            # Compilation?
+            compilation = m.get(t4['partofcompilation'])
+            if not compilation and is_comp:
+                m[t4['partofcompilation']] = True
+                changed = True
+            a_artist = m.get(t4['albumartist'], [None])[0]
+            if compilation and not a_artist:
+                m[t4['albumartist']] = ['Various']
+                changed = True
+        elif 'mp3' in group:
+            m = group['mp3']
+            # Compilation?
+            if is_comp:
+                if not t3['albumartist'] in m or not m[t3['albumartist']].text[0]:
+                    m[t3['albumartist']] = id3.TPE2(0, 'Various')
+                    changed = True
         # Save
         if changed:
             print "  Save {0}".format(file)
             if not dryrun:
-                mp4.save()
+                m.save()
         if name:
             print '  {0}\n    {1}'.format(file, name)
             if not dryrun:
