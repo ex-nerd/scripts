6f8dbf5bcc251626828ccc79ecb221928088936d
Chris Petersen <geek+github@ex-nerd.com>
2011-12-20 23:19:26 -0800

    first pass building m4b audiobook files

---
+++ build_m4b.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+#
+
+# Max length (in seconds) for each segment
+max_length = 60 * 60 * 12
+
+###############################################################################
+
+import os, sys, re
+import subprocess
+import unicodedata
+from mutagen.mp4 import MP4
+
+t4 = {
+    'album':                '\xa9alb',
+    'albumartist':          'aART',
+    'albumartistsortorder': 'soaa',
+    'albumsortorder':       'soal',
+    'artist':               '\xa9ART',
+    'artistsortorder':      'soar',
+    'bpm':                  'tmpo',
+    'comment':              '\xa9cmt',
+    'composer':             '\xa9wrt',
+    'composersortorder':    'soco',
+    'copyright':            'cprt',
+    'description':          'desc',
+    'encodedby':            '\xa9too',
+    'genre':                '\xa9gen',
+    'grouping':             '\xa9grp',
+    'lyrics':               '\xa9lyr',
+    'partofcompilation':    'cpil',
+    'partofgaplessalbum':   'pgap',
+    'podcast':              'pcst',
+    'podcastcategory':      'catg',
+    'podcastepisodeguid':   'egid',
+    'podcastkeywords':      'keyw',
+    'podcasturl':           'purl',
+    'purchasedate':         'purd',
+    'showname':             'tvsh',
+    'showsortorder':        'sosn',
+    'title':                '\xa9nam',
+    'titlesortorder':       'sonm',
+    'year':                 '\xa9day',
+    }
+
+def newburn():
+    return { 'chapters': '', 'tracks': [], 'tlen': 0.0, 'disknum': 0, 'tmin': None, 'tmax': None }
+
+def timestr(secs):
+    (secs, ms) = str(secs).split('.')
+    ms    =  float(ms[0:3] + '.' + ms[3:])
+    secs  = int(secs)
+    hours = int(secs // 3600)
+    secs  = secs % 3600
+    mins  = int(secs // 60)
+    secs  = secs % 60
+    return '{0:02}:{1:02}:{2:02}.{3:03.0f}'.format(hours, mins, secs, ms)
+
+def encode(title, burn, meta):
+    chapterfile = '{0}.chapters.txt'.format(title)
+    outfile = title + '.m4b'
+    # Run mp4box
+    call = ['MP4Box']
+    for tpath in burn['tracks']:
+        call += ['-cat', tpath]
+    call += ['-new', outfile]
+    #print call
+    subprocess.call(call)
+    # Add the appropriate meta tags
+    m = MP4(outfile)
+    m[t4['genre']]    = ['Audiobook']
+    m[t4['title']]    = [title]
+    for key in ('artist', 'album', 'comment', 'year', 'grouping'):
+        if meta[key]:
+            m[t4[key]] = [meta[key]]
+    #'numtracks': numtracks,
+    m.save()
+    # Create the chapters file, chapterize, cleanup
+    with open(chapterfile , 'w') as file:
+        file.write(burn['chapters'])
+    subprocess.call(['mp4chaps', '--chapter-qt', '--import', outfile])
+    os.unlink(chapterfile)
+    # Add a cover image?
+    if os.path.exists('cover.jpg'):
+        subprocess.call(['mp4art', '--remove', outfile])
+        subprocess.call(['mp4art', '--add', 'cover.jpg', outfile])
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        path = os.path.abspath(sys.argv[1])
+    else:
+        path = os.path.abspath('.')
+    print path
+    os.chdir(path)
+    # Load the files
+    files = [filename for filename in os.listdir(".") if filename.endswith(".m4a")]
+    files.sort()
+    # Init
+    burns = []
+    burn = newburn()
+    meta = {}
+    chapters = ''
+    for file in files:
+        m = MP4(file)
+        # Title, for the chapter name
+        title = m[t4['title']][0].strip()
+        # Disk number
+        try:
+            (disknum, numdisks) = m['disk'][0]
+        except:
+            disknum = numdisks = 0
+        if burn['disknum'] and burn['disknum'] != disknum:
+            burns.append(burn)
+            burn = newburn()
+        burn['disknum'] = disknum
+        # Track number
+        (tracknum, numtracks) = m['trkn'][0]
+        if burn['tmin'] == None or int(tracknum) <= int(burn['tmin']):
+            burn['tmin'] = tracknum
+        if burn['tmax'] == None or int(tracknum) >= int(burn['tmax']):
+            burn['tmax'] = tracknum
+        # Other info
+        track = {
+            'artist':    m.get(t4['artist'],   [''])[0].strip(),
+            'album':     m.get(t4['album'],    [''])[0].strip(),
+            'comment':   m.get(t4['comment'],  [''])[0].strip(),
+            'grouping':  m.get(t4['grouping'], [''])[0].strip(),
+            'year':      m.get(t4['year'],     [''])[0].strip(),
+            'disknum':   disknum,
+            'numdisks':  numdisks,
+            'numtracks': numtracks,
+        }
+        # Compare with all-album meta
+        for key in ('artist', 'album', 'comment', 'grouping', 'year', 'numdisks', 'numtracks'):
+            if key in meta:
+                if track[key] != meta[key]:
+                    raise ValueError('Album Mismatch:  {0} != {1}'.format(track[key], meta[key]))
+            else:
+                meta[key] = track[key]
+        # Length and chapter info
+        burn['chapters'] += '{0} {1}\n'.format(timestr(burn['tlen']), title)
+        burn['tlen'] += m.info.length
+        # Add the file
+        burn['tracks'].append(file)
+        # Long enough to break out a chunk?
+        if int(meta.get('numdisks', 0)) < 1 and burn['tlen'] >= max_length:
+            burns.append(burn)
+            burn = newburn()
+    if len(burn['tracks']) > 0:
+        burns.append(burn)
+
+    for burn in burns:
+        if len(burns) == 1:
+            title = meta['album']
+        elif meta['numdisks']:
+            title = "{0} - Disk {1} of {2}".format(meta['album'], burn['disknum'], meta['numdisks'])
+        else:
+            title = "{0} - {1}-{2} of {3}".format(meta['album'], burn['tmin'], burn['tmax'], meta['numtracks'])
+        #print ''
+        #print burn
+        encode(title, burn, meta)
